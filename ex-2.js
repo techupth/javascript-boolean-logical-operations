8abef82dedbd7fcf45c980ba3aec120e8c65786e
fossi13 <fossi13@users.noreply.github.com>
2024-05-09 13:36:22 +0700

    fix:debug code

--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,17 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000;
-let isWeekday;
-let hasBoughtProductFromITCategory;
-let hasAttendedDiscountEvent;
-let isPlatinum;
+let lastMonthPaidMoreThan4000 = true;
+let isWeekday = true;
+let hasBoughtProductFromITCategory = false;
+let hasAttendedDiscountEvent = true;
+let isPlatinum = true;
 
-let hasPromotion;
+let hasPromotion =
+  (lastMonthPaidMoreThan4000 &&
+    isPlatinum &&
+    hasBoughtProductFromITCategory &&
+    hasAttendedDiscountEvent) ||
+  isPlatinum;
 
 console.log(hasPromotion);

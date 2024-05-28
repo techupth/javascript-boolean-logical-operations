117f53b8c84609e43a4c0741dfba02d7b443ea81
zvryn <sarinkanok@gmail.com>
2024-05-28 15:49:00 +0700

    feat: add completed assignment Boolean

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,8 @@
-let isOver18;
-let hasCriminalBlacklist;
-
-let isAllow;
-
+// Exercise #1: Event Conditions
 // Start coding here
-
+let isOver18 = true;
+let hasCriminalBlacklist = false;
+let isAllow = isOver18 && !hasCriminalBlacklist ;
 console.log(isAllow);
+
+

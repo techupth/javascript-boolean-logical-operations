2ac4b31326d49f15571dda56d3503e72451dfac1
thaweesap chantasathanont <thaweesap@192.168.1.35>
2024-09-30 15:34:57 +0700

    Send exercise

---
+++ answer/ex-2.js
@@ -0,0 +1,24 @@
+// Exercise #2: Promotion Conditions
+
+// Start coding here
+
+
+
+
+let memberLevel = "Gold";
+let lastMonthPaid = 4001;
+
+let lastMonthPaidMoreThan4000 = lastMonthPaid > 4000;
+let isWeekday = true;
+let hasBoughtProductFromITCategory = false;
+let hasAttendedDiscountEvent = true;
+let isPlatinum = memberLevel == "Platinum";
+
+let hasPromotion =
+  (lastMonthPaidMoreThan4000 &&
+    isWeekday &&
+    !hasBoughtProductFromITCategory &&
+    !hasAttendedDiscountEvent) ||
+  isPlatinum;
+
+console.log(hasPromotion);
